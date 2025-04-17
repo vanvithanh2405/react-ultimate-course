a07e2407b4753d64815799c75997504364757cf8
Van Vi Thanh <vanvithanh2405@gmail.com>
2025-04-17 18:53:26 +0700

    refine code

--- src/sampleApp/thanh/ThanhColorBox.tsx
+++ src/sampleApp/thanh/ThanhColorBox.tsx
@@ -2,10 +2,10 @@ import { useState } from "react";
 import styles from './css/Main.module.css';
 
 const data = [
-    {id: 1, text: 'red', backgroundColor: 'red'},
-    {id: 2, text: 'yellow', backgroundColor: 'yellow'},
-    {id: 3, text: 'aqua', backgroundColor: 'aqua'},
-    {id: 4, text: 'purple', backgroundColor: 'purple'},
+    { id: 1, text: 'red', backgroundColor: 'red' },
+    { id: 2, text: 'yellow', backgroundColor: 'yellow' },
+    { id: 3, text: 'aqua', backgroundColor: 'aqua' },
+    { id: 4, text: 'purple', backgroundColor: 'purple' },
 ]
 
 
@@ -13,31 +13,22 @@ function ThanhColorBox() {
     const [colors, setColors] = useState(data);
     const [selectedColor, setSelectedColor] = useState('default');
     const [selectedId, setSelectedId] = useState<number | null>(null);
-    
+
     const handleSelectedColor = (id: number, text: string) => {
-        if (selectedId === id) {
-            const resetColors = colors.map((color) => ({
-                ...color,
-                backgroundColor: color.text,
-            }));
-            setColors(resetColors);
-            setSelectedId(null);
-            setSelectedColor('default');
-        } else {
-            const updatedColors = colors.map((color) => ({
-                ...color,
-                backgroundColor: text,
-            }));
-            setColors(updatedColors);
-            setSelectedId(id);
-            setSelectedColor(text);
-        }
+        const updatedColors = colors.map((color) => ({
+            ...color,
+            backgroundColor: selectedId === id ? color.text : text,
+          }));
+          
+          setColors(updatedColors);
+          setSelectedId(selectedId === id ? null : id);
+          setSelectedColor(selectedId === id ? 'default' : text);
     };
 
     return (
-    <>
-        <h1>Color Box</h1>
-        <p>Current color: {selectedColor}</p>
+        <>
+            <h1>Color Box</h1>
+            <p>Current color: {selectedColor}</p>
             <div className={styles.container}>
                 {colors.map((color) => (
                     <div
@@ -45,15 +36,15 @@ function ThanhColorBox() {
                         className={styles.colorBox}
                         style={{ backgroundColor: color.backgroundColor }}
                         onClick={() => {
-                            handleSelectedColor(color.id,color.text);
+                            handleSelectedColor(color.id, color.text);
                         }}
                     >
                         {color.text}
                     </div>
                 ))}
             </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default ThanhColorBox
